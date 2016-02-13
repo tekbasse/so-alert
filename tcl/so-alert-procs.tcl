b7eaab57f3d5acc6c83f58a8bffccf1f1a30f4aa
Benjamin Brink <kappa@dekka.com>
2016-02-13 19:48:01 +0000

    setup lib

---
+++ tcl/so-alert-procs.tcl
@@ -0,0 +1,24 @@
+#so-alert-procs.tcl
+ad_library {
+
+    SO alert package procedures
+    @creation-date 13 Feb 2016
+    @cvs-id $Id:
+    @Copyright (c) 2016 Benjamin Brink
+    @license GNU General Public License 3, see project home or http://www.gnu.org/licenses/gpl-3.0.en.html
+    @project home: http://github.com/tekbasse/so-alert
+    @address: po box 20, Marylhurst, OR 97036-0020 usa
+    @email: tekbasse@yahoo.com
+
+    Temporary comment about git commit comments: http://xkcd.com/1296/
+}
+
+namespace eval so {}
+
+ad_proc -private so::ex {
+    factor_curve_lol
+} {
+    Returns laughter and fun
+} {
+    return $laughter_and_fun
+}
